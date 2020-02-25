1cb3c5b8052d1a54fa3f5c31ff18d38d7c87b62a
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2020-02-24 21:16:20 -0700

    made my own theme and inserted a drawer

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -1,21 +1,37 @@
 <template>
   <nav>
     <v-toolbar flat app>
-      <v-toolbar-title class="text-uppercase grey--text">
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" text color="primary"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text color="grey">
+      <v-btn text color="primary">
         <span>Sign Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer 
+      app 
+      temporary 
+      class='secondary' 
+      v-model='drawer'
+    >
+
+    </v-navigation-drawer>
+
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'navbar'
+  name: 'navbar',
+  data() {
+    return {
+      drawer: false
+    }
+  },
 }
 </script>
