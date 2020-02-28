87945f1b6063305d4641947e7f762b36c7f7b3b0
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2020-02-27 22:24:48 -0700

    added validation to the form

--- todo-ninja/src/components/TodoAddModal.vue
+++ todo-ninja/src/components/TodoAddModal.vue
@@ -5,12 +5,68 @@
     </template>
       <v-card>
         <v-card-title >
-          <h2>Add a New Project</h2>
+          <h2 class="font-weight-light mb-6">Add a New Project</h2>
         </v-card-title>
         <v-card-text>
-          <v-form class="px-10">
-            <v-text-field label="Title" v-model="title"></v-text-field>
-            <v-textarea label="Information" v-model="content"></v-textarea>
+          <v-form 
+            class="px-10" 
+            ref="form"
+            v-model="valid"
+          >
+
+            <v-text-field 
+              label="Title" 
+              v-model="title" 
+              prepend-icon="mdi-format-title"
+              required
+              :counter="20"
+              :rules="nameRules"
+            />
+
+            <v-textarea 
+              label="Information" 
+              v-model="content" 
+              prepend-icon="mdi-lead-pencil"
+              :rules="infoRules"
+              :counter="120"
+              required
+            />
+
+            <v-menu
+              v-model="menu1"
+              :close-on-content-click="false"
+              max-width="290"
+            >
+
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  prepend-icon="mdi-calendar"
+                  :value="computedDateFormattedMomentjs"
+                  clearable
+                  label="Date"
+                  readonly
+                  v-on="on"
+                  @click:clear="date = null"
+                  :rules="dateRules"
+                  required
+                ></v-text-field>
+              </template>
+
+              <v-date-picker
+                v-model="date"
+                @change="menu1 = false"
+              ></v-date-picker>
+
+            </v-menu>
+
+            <v-btn 
+              text 
+              class="primary mt-10 mb-5" 
+              @click="submit"
+              :disabled="!valid"
+            >
+            Add Project
+            </v-btn>
           </v-form>
         </v-card-text>
       </v-card>
@@ -18,12 +74,40 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   data() {
     return {
       title: '',
       content: '',
+      date: new Date().toISOString().substr(0, 10),
+      menu1: false,
+      valid: true,
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 20) || 'Name must be less than 20 characters',
+      ],
+      infoRules: [
+        v => !!v || 'Information is required',
+        v => (v && v.length <= 120) || 'Information must be less than 120 characters',
+      ],
+      dateRules: [
+        v => !!v || 'Date is required',
+      ],
     }
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+      }
+    },
+  },
+  computed: {
+    computedDateFormattedMomentjs () {
+      return this.date ? moment(this.date).format(' MMMM Do YYYY') : ''
+    },
   }
 }
 </script>
--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -66,16 +66,16 @@ export default {
   data() {
     return {
       projects: [
-        {title: "Learn Vue", person: "Pete Kurjanowicz", due: "Jan 1st 2020", status: "Complete", CSS: "complete"},
-        {title: "Learn Docker", person: "Pete Kurjanowicz", due: "Jan 15th 2020", status: "Over Due", CSS: "overdue"},
-        {title: "Learn Vuetify", person: "Pete Kurjanowicz", due: "Feb 29th 2020", status: "In Progress", CSS: "inprogress"},
-        {title: "Learn Node.js", person: "Pete Kurjanowicz", due: "Mar 10th 2020", status: "Not Started", CSS: "notstarted"},
-        {title: "Build Framework for Jess' website", person: "Pete Kurjanowicz", due: "Mar 20th 2020", status: "Not Started", CSS: "notstarted"},
-        {title: "Deploy Jess' Recipe Website", person: "Pete Kurjanowicz", due: "Apr 30th 2020", status: "Not Started", CSS: "notstarted"},
-        {title: "Break office chairs", person: "Ivana Humpalot", due: "Apr 12th 2020", status: "In Progress", CSS: "inprogress"},
-        {title: "Destroy happy marriages", person: "Ivana Humpalot", due: "Mar 14th 2020", status: "Over Due", CSS: "overdue"},
-        {title: "Order Pizza", person: "Mike Hunt", due: "Apr 30th 2020", status: "Complete", CSS: "complete"},
-        {title: "Eat that pizza ;)", person: "Mike Hunt", due: "Mar 2nd 2020", status: "In Progress", CSS: "inprogress"},
+        {title: "Learn Vue", person: "Pete Kurjanowicz", due: "January 1st 2020", status: "Complete", CSS: "complete", content: "Learn the Vue.js front end framework. This will help improve the speed in which I can deploy applications"},
+        {title: "Learn Docker", person: "Pete Kurjanowicz", due: "January 15th 2020", status: "Over Due", CSS: "overdue", content: "Talk to Chris about this, this will help with deployment"},
+        {title: "Learn Vuetify", person: "Pete Kurjanowicz", due: "Feburary 29th 2020", status: "In Progress", CSS: "inprogress", content: "This will improve the look and feel of my websites"},
+        {title: "Learn Node.js", person: "Pete Kurjanowicz", due: "March 10th 2020", status: "Not Started", CSS: "notstarted", content: "This will allow me to use websockets to deploy Jess' recipe app"},
+        {title: "Build Framework for Jess' website", person: "Pete Kurjanowicz", due: "March 20th 2020", status: "Not Started", CSS: "notstarted", content: "Start Building :)"},
+        {title: "Deploy Jess' Recipe Website", person: "Pete Kurjanowicz", due: "April 30th 2020", status: "Not Started", CSS: "notstarted", content: "Can't wait"},
+        {title: "Break office chairs", person: "Ivana Humpalot", due: "April 12th 2020", status: "In Progress", CSS: "inprogress", content: "Lets get it poppin"},
+        {title: "Destroy happy marriages", person: "Ivana Humpalot", due: "March 14th 2020", status: "Over Due", CSS: "overdue", content: "No very nice..."},
+        {title: "Order Pizza", person: "Mike Hunt", due: "April 30th 2020", status: "Complete", CSS: "complete", content: "I love pizza"},
+        {title: "Eat that pizza ;)", person: "Mike Hunt", due: "March 2nd 2020", status: "In Progress", CSS: "inprogress", content: "I love eating pizza"},
       ]
     }
   },
